d176c8b7cbe21a67b73ec13cd74663bdf8b45769
JOR <john.oraw@atu.ie>
2025-11-20 20:30:23 +0000

    Added budget.py

---
+++ Python/Exercise1/slicing.py
@@ -0,0 +1,22 @@
+```
+Script: FileName.py
+By: JOR
+Purpose:
+Prerequisites:
+Tested: on Python v3.10.4 under Windows 10
+Revision History:
+Alpha version: 
+```
+
+# slicing.py
+
+a = "Greetings!"
+
+# Example in the notes
+#print(a[0:6:2])
+
+# This works cos...
+# print(a[-1:-5:-1])
+
+# Pick out a single character
+print(a[4])
